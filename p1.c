4b44c8a9b02d444a1578068153b98f047777105b
Ravindu Herath <122708614+hercul3poirot@users.noreply.github.com>
2023-01-20 20:44:31 +1100

    Add files via upload

--- p1.c
+++ p1.c
@@ -1,8 +1,9 @@
 //1. Write a C program to print your name, date of birth. and mobile number.
 #include <stdio.h>
 
-void main() {
+int main() {
     printf("Name   : Alexandra Abramov\n");
     printf("DOB    : July 14, 1975\n");
     printf("Mobile : 99-9999999999\n");
+    return 0;
 }
